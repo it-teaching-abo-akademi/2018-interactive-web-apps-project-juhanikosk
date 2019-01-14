f9226662c927e05b047004b428cfcb8317b6f63e
juhani.koskinen1@edu.turkuamk.fi <juhanikos@gmail.com>
2019-01-14 20:03:40 +0200

    Updates

---
+++ precache-manifest.e3d8d7788bc58e7e21de81d78bd587a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/2018-interactive-web-apps-project-juhanikosk/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "e0d576f523c7e0724058",
+    "url": "/2018-interactive-web-apps-project-juhanikosk/static/js/runtime~main.e0d576f5.js"
+  },
+  {
+    "revision": "70ae9024ea4cd95ad7c1",
+    "url": "/2018-interactive-web-apps-project-juhanikosk/static/js/main.70ae9024.chunk.js"
+  },
+  {
+    "revision": "4a5897b8a9dd010b4a35",
+    "url": "/2018-interactive-web-apps-project-juhanikosk/static/js/1.4a5897b8.chunk.js"
+  },
+  {
+    "revision": "70ae9024ea4cd95ad7c1",
+    "url": "/2018-interactive-web-apps-project-juhanikosk/static/css/main.8b970697.chunk.css"
+  },
+  {
+    "revision": "8c23aef17b1bbac10a9f2c858cdc9e7d",
+    "url": "/2018-interactive-web-apps-project-juhanikosk/index.html"
+  }
+];
