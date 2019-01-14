2f9b78bb245deb169b0ea5db080da5693e458b10
juhani.koskinen1@edu.turkuamk.fi <juhanikos@gmail.com>
2019-01-14 20:19:10 +0200

    Updates

--- precache-manifest.ae133ba52bbd34540e0e59e519acc683.js
+++ precache-manifest.ae133ba52bbd34540e0e59e519acc683.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/2018-interactive-web-apps-project-juhanikosk/static/js/runtime~main.e0d576f5.js"
   },
   {
-    "revision": "70ae9024ea4cd95ad7c1",
-    "url": "/2018-interactive-web-apps-project-juhanikosk/static/js/main.70ae9024.chunk.js"
+    "revision": "e088932c8e4f6f7f688e",
+    "url": "/2018-interactive-web-apps-project-juhanikosk/static/js/main.e088932c.chunk.js"
   },
   {
     "revision": "4a5897b8a9dd010b4a35",
     "url": "/2018-interactive-web-apps-project-juhanikosk/static/js/1.4a5897b8.chunk.js"
   },
   {
-    "revision": "70ae9024ea4cd95ad7c1",
-    "url": "/2018-interactive-web-apps-project-juhanikosk/static/css/main.8b970697.chunk.css"
+    "revision": "e088932c8e4f6f7f688e",
+    "url": "/2018-interactive-web-apps-project-juhanikosk/static/css/main.45c739de.chunk.css"
   },
   {
-    "revision": "8c23aef17b1bbac10a9f2c858cdc9e7d",
+    "revision": "7a9e1de5272058c7d368475b05c029f4",
     "url": "/2018-interactive-web-apps-project-juhanikosk/index.html"
   }
 ];
