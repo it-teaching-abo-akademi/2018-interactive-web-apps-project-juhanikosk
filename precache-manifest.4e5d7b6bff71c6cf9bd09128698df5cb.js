ee2c64a7c307a8accfd2323cf1a083a4097f738e
juhani.koskinen1@edu.turkuamk.fi <juhanikos@gmail.com>
2019-01-14 20:01:03 +0200

    Updates

---
+++ precache-manifest.4e5d7b6bff71c6cf9bd09128698df5cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "35ec2ff48768edff8ae3",
+    "url": "/static/js/main.35ec2ff4.chunk.js"
+  },
+  {
+    "revision": "aac75bbadb65a070ad02",
+    "url": "/static/js/1.aac75bba.chunk.js"
+  },
+  {
+    "revision": "35ec2ff48768edff8ae3",
+    "url": "/static/css/main.723e140e.chunk.css"
+  },
+  {
+    "revision": "98a3efea39a1ad2804deac1894ae2b36",
+    "url": "/index.html"
+  }
+];
